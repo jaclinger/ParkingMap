3e8adc506235c558b7e9798c4402ecfeb26491aa
Joseph Clinger <156875433+jaclinger@users.noreply.github.com>
2026-04-09 21:23:49 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -10,9 +10,30 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 fetch("parking.geojson")
     .then(response => response.json())
     .then(data => {
-        L.geoJSON(data, {
-            onEachFeature: function (feature, layer) {
 
+        function styleFeature(feature) {
+            const type = feature.properties.type;
+
+            let color = "#3388ff"; // default blue (Student)
+
+            if (type === "Faculty") {
+                color = "#ff4d4d"; // red
+            } 
+            else if (type === "Visitor") {
+                color = "#4dff88"; // green
+            }
+
+            return {
+                color: color,
+                weight: 2,
+                fillOpacity: 0.4
+            };
+        }
+
+        L.geoJSON(data, {
+            style: styleFeature,
+
+            onEachFeature: function (feature, layer) {
                 const props = feature.properties;
 
                 layer.bindPopup(`
